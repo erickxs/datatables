16404361f2f86e3efbd59d0ddb6e71278534c317
Erick LS <erickdl.xs@gmail.com>
2021-03-29 14:02:46 -0600

    Implementacion de DataTables

--- app/config.php
+++ app/config.php
@@ -1,2 +1,11 @@
 <?php
 // Definir constantes para la Base de Datos
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'datatables');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_OPTIONS', array(
+  PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+  PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4"
+));
--- index.php
+++ index.php
@@ -1,3 +1,26 @@
+<?php
+
+require_once 'app/config.php';
+
+try {
+  $db = new PDO(
+    "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . "",
+    DB_USER,
+    DB_PASS,
+    DB_OPTIONS
+  );
+
+  $stmt = $db->prepare('SELECT p.id, p.name, p.slug, p.excerpt, p.status, p.file, u.name AS author FROM posts AS p INNER JOIN users as u ON p.user_id = u.id ORDER BY p.id ASC;');
+
+  $stmt->execute();
+
+  $posts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+  echo "Error: " . $e->getMessage();
+  die();
+}
+
+?>
 <!doctype html>
 <html lang="es">
 
@@ -8,10 +31,11 @@
   <!-- Favicon -->
   <link rel="shortcut icon" href="assets/favicon.png" type="image/x-icon">
   <!-- Bootstrap -->
-
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
   <!-- DataTables -->
-
+  <link rel="stylesheet" href="assets/plugins/datatables/datatables.min.css">
   <!-- FontAwesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
 
   <!-- Custom CSS -->
   <link rel="stylesheet" href="assets/css/style.css">
@@ -33,7 +57,51 @@
     <h3 class="text-center mb-5">Lado Cliente</h3>
     <div class="row">
       <div class="col-12">
-        <!-- Aquí empieza el contenido de la tabla -->
+        <table id="postsTable" class="table table-striped table-bordered responsive" style="width:100%">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Nombre</th>
+              <th>Ruta</th>
+              <th>Estado</th>
+              <th>Autor</th>
+              <th>Imagen</th>
+              <th>Acciones</th>
+            </tr>
+          </thead>
+          <?php
+
+          foreach ($posts as $key => $value) : ?>
+
+            <tr>
+              <td><?= $value["id"] ?></td>
+              <td><?= $value["name"] ?></td>
+              <td><?= $value["slug"] ?></td>
+              <td><button type="button" class="btn btn-block <?= ($value["status"] == 'PUBLISHED') ? 'btn-success' : 'btn-danger' ?>"><?= ($value["status"] == 'PUBLISHED') ? 'PUBLICADO' : 'BORRADOR' ?></button></td>
+              <td><?= $value["author"] ?></td>
+              <td><img src=" <?= $value["file"] ?>" style="width:100px;"></td>
+              <td>
+                <div class="btn-group" role="group" aria-label="Action buttons">
+                  <button type="button" class="btn btn-primary" data-id="<?= $value['id'] ?>"><i class="far fa-edit text-white"></i></button>
+                  <button type="button" class="btn btn-danger deleteBtn" data-id="<?= $value['id'] ?>"><i class="fas fa-trash text-white"></i></button>
+                </div>
+              </td>
+            </tr>
+
+          <?php endforeach ?>
+
+          <tfoot>
+            <tr>
+              <th>ID</th>
+              <th>Nombre</th>
+              <th>Ruta</th>
+              <th>Estado</th>
+              <th>Autor</th>
+              <th>Imagen</th>
+              <th>Acciones</th>
+            </tr>
+          </tfoot>
+        </table>
       </div>
     </div>
   </main>
@@ -55,6 +123,13 @@
     </div>
   </footer>
 
+
+  <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
+  <!-- DataTables -->
+  <script src="assets/plugins/datatables/datatables.min.js"></script>
+  <!-- Custom JS -->
   <script src="assets/js/posts.js"></script>
 </body>
 
